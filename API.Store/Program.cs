fa58c543458a742100b402da95a03004bd20d2b7
floresthomas <thomyeze57@gmail.com>
2024-05-05 15:03:45 -0300

    All finished

--- API.Store/Program.cs
+++ API.Store/Program.cs
@@ -103,11 +103,8 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
 
     app.UseHttpsRedirection();
 
